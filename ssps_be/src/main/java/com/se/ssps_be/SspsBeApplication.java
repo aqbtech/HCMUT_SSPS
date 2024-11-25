e36abc027fabf57a81769deb3632fd0062fb45b6
Tu Van Nguyen Anh Quan <tvnaquan@gmail.com>
2024-11-26 02:08:53 +0700

    initial commit

---
+++ ssps_be/src/main/java/com/se/ssps_be/SspsBeApplication.java
@@ -0,0 +1,13 @@
+package com.se.ssps_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SspsBeApplication.class, args);
+	}
+
+}
---
+++ ssps_be/src/test/java/com/se/ssps_be/SspsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.ssps_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SspsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
