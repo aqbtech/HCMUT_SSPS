e96c0b5d8fc8af4f4439a08f7df8da9e990e0f0f
Tran Hung Quoc <quoc911ndu1819@gmail.com>
2024-11-28 01:04:36 +0700

    fe set-up

--- ssps_fe/src/components/Layouts/Layout1/index.js
+++ ssps_fe/src/components/Layouts/Layout1/index.js
@@ -1,2 +1,16 @@
-import Layout1 from './Layout1.css';
+import './Layout1.css';
+import { Outlet } from "react-router-dom";
 
+function Layout1() {
+  return (
+    <>
+      <div className="main-default-layout">
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default Layout1;
